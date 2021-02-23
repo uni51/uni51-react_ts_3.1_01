8913353005bece8e6523e348fd3c09435457f302
uni51 <unieye51@gmail.com>
2021-02-23 14:13:43 +0900

    04: rest-destruct.js

---
+++ 04-function/ rest-params.js
@@ -0,0 +1,10 @@
+const showNames = (a, b, ...rest) => {
+  console.log(a);
+  console.log(b);
+  console.log(rest);    
+}
+
+showNames('John', 'Jane', 'Johnny', 'Jenny', 'Julia');
+// John
+// Jane
+// ['Johnny', 'Jenny', 'Julia']
---
+++ 04-function/rest-destruct.js
@@ -0,0 +1,5 @@
+const sum = (i, ...[j, k, l]) => i + j + k + l;
+
+console.log(sum(1, 2, 3, 4)); // 10
+
+console.log(sum(1, 1, 1, 1, 1, 1))
